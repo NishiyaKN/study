a5316d2601b75f89760931176268cf71215eb326
KenichiNishiya <yorimitsu@proton.me>
2023-08-11 08:15:49 -0300

    upload files

---
+++ java/test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class test{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um numero inteiro");
+        int numero = input.nextInt();
+
+        System.out.println("Voce digitou o numero: " + numero);
+        // int a = 5;
+        // int b = 19;
+        // if(b < 10){
+        // System.out.println("menor q 10");
+        // }
+        // else{
+        // System.out.println("maior q 10");
+        // }
+    }
+}
