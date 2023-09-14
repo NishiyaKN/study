f0dc3fa8668b249510a60af590325860b84a8cdc
KenichiNishiya <yorimitsu@proton.me>
2023-09-13 21:35:06 -0300

    Upload files

--- py/altissia.py
+++ py/altissia.py
@@ -1,7 +1,18 @@
-
 import pyautogui 
 import time
+i = 0
 
 while True:
+
+    currentMouseX, currentMouseY = pyautogui.position()
+    pyautogui.moveTo(530,368)
     pyautogui.click()
-    time.sleep(2)
+    pyautogui.moveTo(currentMouseX,currentMouseY)
+    if i == 0:
+        pyautogui.move(1,0, duration=0.1)
+        i=1
+    else:
+        pyautogui.move(-1,0, duration=0.1)
+        i=0
+
+    time.sleep(5)
