80d99f9d867bd820b0df2b8752e980f62658090a
KenichiNishiya <yorimitsu@proton.me>
2023-09-22 10:41:44 -0300

    Upload files

---
+++ c/ponteiros/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("Inicio do programa\n");
+    int a = 5;
+    int * p = NULL;
+
+    p = &a; // ponteiro p recebe o endereco de a
+
+    printf("Conteudo de a: %d\n", *p);
+    printf("Fim do programa\n");
+}
