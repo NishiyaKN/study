e4cd7e3af7670125657a3e7ea653372d3cd62e78
Kenichi Nishiya <yorimitsu@proton.me>
2023-12-03 16:15:14 -0300

    Upload files

--- py/soup/new_banner.py
+++ py/soup/new_banner.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-filename = '/home/yori/study/py/soup/counter_normal.txt'
-auth_file = '/home/yori/.config/tk/dc'
+FILENAME = '/home/yori/study/py/soup/counter_normal.txt'
+AUTH_FILE = '/home/yori/.config/tk/dc'
 
 def main():
+    # Gets the page html and the table lenght, use it to compare with the last table lenght registered
     url = 'https://bluearchive.wiki/wiki/Banner_List_(Global)'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
@@ -17,13 +18,14 @@ def main():
     query_counter(counter_today)
 
 def query_counter(counter_today):
+    # Create file to store table length if not exists
     try:
-        with open(filename,"x") as f:
-            print(f"Creating {filename} file...")
+        with open(FILENAME,"x") as f:
+            print(f"Creating {FILENAME} file...")
     except Exception:
         pass
 
-    with open(filename, "r") as f:
+    with open(FILENAME, "r") as f:
         global file_counter
         file_counter = f.read()
         # if file_counter is empty, it will throw an error trying to parse to int
@@ -37,8 +39,9 @@ def query_counter(counter_today):
             print("Nothing to update")
 
 def update_counter(counter_today):
+    # Write new value and create a DataFrame
     try:
-        with open(filename, 'w') as f:
+        with open(FILENAME, 'w') as f:
             f.write(str(counter_today))
             print("Updated counter")
     except Exception:
@@ -46,12 +49,14 @@ def update_counter(counter_today):
     create_dataframe()
 
 def create_dataframe():
+    # Gets table's columns name
     table_headers = table.find_all('th') # returns a bs4 object
     table_headers_text = [title.text.strip() for title in table_headers] # returns string list
 
     global df
     df = pd.DataFrame(columns = table_headers_text)
 
+    # Insert each row to the DataFrame, starts by the index 1 since index 0 is the headers (column names)
     for row in table_rows[1:]:
         row_data = row.find_all('td')
         individual_row_data = [data.text.strip() for data in row_data]
@@ -63,20 +68,24 @@ def create_dataframe():
     # print(df.loc[file_counter:])
 
 def notify_discord():
-    with open(auth_file,'r') as f:
+    # Read authorization token from local file
+    with open(AUTH_FILE,'r') as f:
         auth = f.read()
+
     url = 'https://discord.com/api/v9/channels/1180832500403155095/messages' 
     headers = {
             "Authorization" : auth.strip()
     }
+
+    # Formulate the text string to be sent
     for i in range(file_counter, counter_today - 1):
         character = df.loc[i]['Character'].rstrip('rerun')
         period = df.loc[i]['Period']
         payload = {
                 "content": "New banner: " + character + ". Period: " + period
         }
+
         res = requests.post(url, payload, headers=headers)
 
 if __name__ == '__main__':
     main()
-
